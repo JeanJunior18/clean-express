c3a9b49a032de8338bb2d178e796f219875e2633
Jean Charles <jclgjunior@gmail.com>
2024-11-07 16:50:03 -0300

    feat: swagger

--- src/app.js
+++ src/app.js
@@ -1,9 +1,27 @@
 import express from 'express'
 import CanonicalMessageController from './controller/canonical-message.controller.js'
+import swagger from 'swagger-ui-express'
+import swaggerJSDoc from 'swagger-jsdoc'
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'My API',
+      version: '1.0.0',
+      description:
+        'API documentation for my Express app using Clean Architecture.'
+    }
+  },
+  apis: ['./src/controller/*.js']
+}
 
 const app = express()
 app.use(express.json())
+
 app.use('/message', CanonicalMessageController)
+const swaggerSpec = swaggerJSDoc(options)
+app.use('/api', swagger.serve, swagger.setup(swaggerSpec))
 
 app.listen(3000, () => {
   console.log('Server is running on PORT 3000')
--- src/controller/canonical-message.controller.js
+++ src/controller/canonical-message.controller.js
@@ -5,6 +5,45 @@ import KafkaEventPublisher from '../infrastructure/kafka/kafka-event-publisher.j
 const router = Router()
 const eventEmitter = new KafkaEventPublisher()
 
+/**
+ * @swagger
+ * tags:
+ *   - name: Kafka
+ *     description: Endpoints related to Kafka messaging
+ * /publish:
+ *   post:
+ *     summary: Publish a message on Kafka
+ *     tags: [Kafka]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             additionalProperties: true  # Permite propriedades adicionais de forma livre
+ *           example:
+ *             key1: "value1"
+ *             key2: "value2"
+ *     responses:
+ *       200:
+ *         description: Message published successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 topic:
+ *                   type: string
+ *                   example: "CANONICAL_MESSAGE"
+ *                 data:
+ *                   type: object
+ *                   additionalProperties: true  # Permite dados livres dentro de "data"
+ *             example:
+ *               topic: "CANONICAL_MESSAGE"
+ *               data:
+ *                 key1: "value1"
+ *                 key2: "value2"
+ */
 router.post('/publish', async (req, res) => {
   const publishCanonicalMessage = new PublishCanonicalMessage(eventEmitter)
   const message = await publishCanonicalMessage.execute(req.body)
